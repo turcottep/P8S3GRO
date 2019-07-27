fd7b2b91d6ed91928821fa44d01e5698ef2fadc5
FAT8888 <felix-antoine.toussaint@usherbrooke.ca>
2019-07-27 11:41:15 -0400

    Update main.cpp

--- IdentificationParams/identificationArduino/src/main.cpp
+++ IdentificationParams/identificationArduino/src/main.cpp
@@ -65,13 +65,14 @@ float p6 = 1;
 float vit = 0;
 float breakVit = 0;
 float distanceInit;
-float vMax = .5;
+float vMax = .4;
 float angle_;
 float angle_go;
 float longueur = 69;
 float encodeurInit = 0;
 int state = 1; //machine à états
 int stateOscille = 1; //machine à états oscillation
+int stateOscillePas = 1;
 bool ready_set = false;
 
 /*------------------------- Prototypes de fonctions -------------------------*/
@@ -105,7 +106,8 @@ void resetEncodeur(){encodeurInit = -vexEncoder_.getCount();}
 bool limSwitch(){return analogRead(SWITCH_PIN)>1000;}
 
 void oscilleSIN(float hauteur);
-bool oscilleFAT2(float distance, float hauteur);
+bool oscilleFAT2(float distance, float hauteur); // Pour faire osciller le pendule avec le sapin
+bool oscillepasFAT2(float distance); // Pour ne pas faire osciller le pendule
 void crisseLeCampFAT();
 void securiteFAT();
 
@@ -135,21 +137,21 @@ void setup() {
   timerPulse_.setCallback(endPulse);
   
   // Initialisation du PID
-  pidPos_.setGains(0.001,0 ,0);
-    // Attache des fonctions de retour
-  pidPos_.setMeasurementFunc(getEncoder);
-  pidPos_.setCommandFunc(PIDcommand);
-  pidPos_.setAtGoalFunc(PIDgoalReached);
-  pidPos_.setEpsilon(10);
-  pidPos_.setPeriod(100);
+  // pidPos_.setGains(0.001,0 ,0);
+  //   // Attache des fonctions de retour
+  // pidPos_.setMeasurementFunc(getEncoder);
+  // pidPos_.setCommandFunc(PIDcommand);
+  // pidPos_.setAtGoalFunc(PIDgoalReached);
+  // pidPos_.setEpsilon(10);
+  // pidPos_.setPeriod(100);
 
-  pidAngle_.setGains(-0.01,0 ,0);
-    // Attache des fonctions de retour
-  pidAngle_.setMeasurementFunc(getAngleDegre);
-  pidAngle_.setCommandFunc(PIDcommand);
-  pidAngle_.setAtGoalFunc(PIDgoalReached);
-  pidAngle_.setEpsilon(10);
-  pidAngle_.setPeriod(100);
+  // pidAngle_.setGains(-0.01,0 ,0);
+  //   // Attache des fonctions de retour
+  // pidAngle_.setMeasurementFunc(getAngleDegre);
+  // pidAngle_.setCommandFunc(PIDcommand);
+  // pidAngle_.setAtGoalFunc(PIDgoalReached);
+  // pidAngle_.setEpsilon(10);
+  // pidAngle_.setPeriod(100);
 
   resetAngle();
   pinMode(MAGPIN,OUTPUT);
@@ -159,7 +161,6 @@ void setup() {
   digitalWrite(53,HIGH);
   pinMode(SWITCH_PIN,INPUT);
 
-
   delay(1000);
   // distanceTest2(-20, .1);
   // distanceTest2(0, .1);
@@ -167,7 +168,6 @@ void setup() {
 
 void loop() {
   
-
   // Serial.print("state = ");
   // Serial.println(state);
 
@@ -178,46 +178,63 @@ void loop() {
       break;
       
     case 1:
-      //recule et attrape le sapin
+      // recule et attrape le sapin
       setMoteurs(-0.1);
-      activateMag(1);
       if(limSwitch()){
+        setMoteurs(0);
+        activateMag(1);
         resetAngle();
         resetEncodeur();
+        delay(1000);
         state = 2;
       } 
       break;
     
     case 2:
-      //avance et se positionne
+      // avance et se positionne
       if(distanceTest2(10,vMax))state = 3;
-      stateOscille = 1;
+      stateOscille = 1;  // *** ATTENTION si vous modifiez cette ligne le robot peut crisser le camp par terre et détruire une roue!
       break;
 
     case 3:
-      //oscille
-      if(oscilleFAT2(5,170))state = 0;
+      // oscille
+      if(oscilleFAT2(2,140))state = 4;
       break;
 
     case 4:
-      //avance au panier
-      if (distanceTest2(100,vMax))state = 5;
+      // avance au panier
+      if (distanceTest2(120,.8))state = 5;
       break;
 
     case 5:
+      setMoteurs(0);
+      // while (1){
+      //   double angle_pendule = getAngle();
+      //   if (angle_pendule < -90 && angle_pendule > -70){
+      //     if (getAngle() < -70 && getAngle() > -50){
+      //       activateMag(0);
+      //       break;
+      //     }
+      //   }
+      // }
+      activateMag(0);
+      setMoteurs(-0.1);
+      if(limSwitch()){
+        state = 0;
+      } 
       //setup PID
-      pidPos_.setGoal(cmToTick(100));
-      pidAngle_.setGoal(0);
-      pidPos_.enable();
-      pidAngle_.enable();
-      state = 6;
+      // pidPos_.setGoal(cmToTick(100));
+      // pidAngle_.setGoal(0);
+      // pidPos_.enable();
+      // pidAngle_.enable();
+      // state = 6;
       break;
 
     case 6:
       //stabilise
-      pidPos_.run(0);
-      pidAngle_.run(1);
-      if (pidPos_.isAtGoal() && pidAngle_.isAtGoal())  state = 0;
+      // pidPos_.run(0);
+      // pidAngle_.run(1);
+      // if (pidPos_.isAtGoal() && pidAngle_.isAtGoal())  state = 0;
       break;
 
     default:
@@ -254,12 +271,10 @@ void crisseLeCampFAT(){
   do
   {
     securiteFAT();
-
     AX_.setMotorPWM(0,-vMax);
     AX_.setMotorPWM(1,-vMax);
 
   } while (true);
-  
 }
 
 bool oscilleFAT2(float distanceMax, float angleDegre){
@@ -300,13 +315,12 @@ bool oscilleFAT2(float distanceMax, float angleDegre){
         angle_ = getAngle();
       }else
       {
+        if (angle_ >= 90) delay(1250);
         stateOscille = 2;
       }
     break;
 
-
-  default:
-
+    default:
     break;
   }
   
@@ -321,10 +335,26 @@ bool oscilleFAT2(float distanceMax, float angleDegre){
   Serial.print(angle_go);
   Serial.print(" | ready:");
   Serial.println(ready_set);
-
   return getAngleDegre() > angleDegre;
 
 }
+
+// bool oscillepasFAT2(float distance){
+  
+//   switch (stateOscillePas)
+//   {
+//     case 1:
+//     SetMoteur(0);
+//     break;
+
+//     case 2:
+//     SetMoteur(0);
+//     break;
+    
+//     default:
+//     break;
+//   }
+// }
 
 
 bool distanceTest2(float distanceCM, float vit){
@@ -369,9 +399,9 @@ void Stop(){
 // Fonctions pour le PID
 
 void PIDcommand(){
-  double cmd = pidAngle_.getCmd();
-  cmd += 0.7 * pidPos_.getCmd();
-  setMoteurs(cmd);
+  // double cmd = pidAngle_.getCmd();
+  // cmd += 0.7 * pidPos_.getCmd();
+  // setMoteurs(cmd);
 }
 
 void PIDgoalReached(){
